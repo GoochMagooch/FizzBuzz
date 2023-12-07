3c0fceba0eb5534329ae70ab6f68894571f337f4
Magooch <goochmagooch@gmail.com>
2023-12-06 18:04:16 -0700

    Place if else statement within for loop

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ const output = document.querySelectorAll('.output');
 
 function fizzbuzz() {
     if (num.value >= 1) {
-        for (i = 1, i <= num.value, i++)
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log('FizzBuzz');
-        } else if (i % 5 == 0) {
-            console.log('Buzz');
-        } else if (i % 3 == 0) {
-            console.log('Fizz');
-        } else {
-            console.log(i);
+        for (i = 1, i <= num.value, i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                console.log('FizzBuzz');
+            } else if (i % 5 == 0) {
+                console.log('Buzz');
+            } else if (i % 3 == 0) {
+                console.log('Fizz');
+            } else {
+                console.log(i);
+            }
         }
     } else {
         alert('enter a number please');
