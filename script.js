90987e08ad4cca4824a08e6fbd6d35e06ff75e4e
Magooch <goochmagooch@gmail.com>
2023-12-06 18:00:55 -0700

    Write FizzBuzz script

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const num = document.querySelectorAll('.num');
 const btn = document.querySelectorAll('.button');
 const output = document.querySelectorAll('.output');
 
+function fizzbuzz() {
+    if (num.value >= 1) {
+        for (i = 1, i <= num.value, i++)
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log('FizzBuzz');
+        } else if (i % 5 == 0) {
+            console.log('Buzz');
+        } else if (i % 3 == 0) {
+            console.log('Fizz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+btn.addEventListener('click', fizzbuzz);
